af03f46ef184615251c0fc2af7b4eb701e0f4815
root <root@localhost.localdomain>
2014-12-19 11:59:26 +0800

    start OMdemo

---
+++ shell/autoreload.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+objectdir="/data/www/OMdemo"
+/usr/local/bin/inotifywait -mrq --exclude "(static|logs|shell|\.swp|\.swx|\.pyc|\.py\~)" --timefmt '%d/%m/%y %H:%M' --format '%T %w%f' --event modify,delete,move,create,attrib ${objectdir} | while read files
+do
+        /bin/touch /data/www/OMdemo/shell/reload.file
+        continue
+done &
---
+++ shell/readme.txt
@@ -0,0 +1,2 @@
+/usr/bin/uwsgi --ini /usr/local/nginx/conf/uwsgi.ini --touch-reload=/data/www/OMdemo/shell/reload.file
+/usr/local/nginx/sbin/nginx
